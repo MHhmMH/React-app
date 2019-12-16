2ff3fdf003e68a21bf093155b6c148fcf0e035c9
hanminghao <hmh950728@126.com>
2019-12-15 19:08:29 -0800

    finish season app

--- season/src/index.js
+++ season/src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {lat: null, errorMessage : ''};
+  state = {lat : null, errorMessage : '' }
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition (
-      position => {
-        this.setState({lat : position.coords.latitude});
-      },
-      err => {
-        this.setState({errorMessage : err.errorMessage});
-      }
+      position => this.setState({lat : position.coords.latitude}),
+      err => this.setState({errorMessage : err.errorMessage})
     );
   }
   render () {
-    if (this.state.lat && ! this.state.errorMessage) {
-      return <div> latitude : {this.state.lat} </div>
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />
      }
      if (!this.state.lat && this.state.errorMessage) {
        return <div> error : {this.state.errorMessage} </div>
